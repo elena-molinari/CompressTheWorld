792252e9be88d33688946ebed425bfdf5e020f2a
starpa2810 <paolostella28@gmail.com>
2024-11-25 18:33:04 +0100

    3 upgrade! No latency_ just one compressor

--- src/script.js
+++ src/script.js
@@ -8,6 +8,13 @@ let df_ratio = 12;
 let df_att = 0.003;
 let df_rel = 0.25; 
 
+createCompressor();  //devo creare un unico compressore! una sola volta.
+//Quando chiami createCompressor(), crei un nuovo oggetto DynamicsCompressorNode tramite c.createDynamicsCompressor(). Questo nodo è un componente che vive all'interno del grafo di elaborazione audio di AudioContext
+// se metto questa riga all'interno del main ogni volta che richiamo la funzione creo un nuovo nodo completamente separato dal precedente.
+// Quindi commetto due errore:
+// 1)Stai solo creando un nuovo compressore, ma non ricolleghi l'oscillatore al nuovo compressore.
+//  2)Se il compressore precedente è ancora connesso, potresti accumulare nodi inutilizzati nel grafo audio, causando un comportamento inatteso e un consumo di risorse non necessario.
+//In breve: il compressore è un oggetto che deve essere creato una volta e riutilizzato, evitando duplicazioni.
 
 function main() {
     c.resume(); // Assicurati che l'AudioContext sia attivo
@@ -15,7 +22,7 @@ function main() {
         // Crea un nuovo oscillatore
         oscillator = c.createOscillator();
         oscillator.connect(c.destination);
-        createCompressor();
+        
         compOnOff(state_comp);
         oscillator.start(); // Avvia l'oscillatore
     }
@@ -53,11 +60,31 @@ function compOnOff(state_comp) {
 
 function createCompressor() {
     compressor = c.createDynamicsCompressor();
-    compressor.threshold.setValueAtTime(df_th, c.currentTime);  // Imposta la soglia
-    compressor.knee.setValueAtTime(df_knee, c.currentTime);       // Imposta il knee
+    compressor.threshold.setValueAtTime(df_th, c.currentTime );  // Imposta la soglia
+    compressor.knee.setValueAtTime(df_knee, c.currentTime );       // Imposta il knee
     compressor.ratio.setValueAtTime(df_ratio, c.currentTime);      // Imposta il rapporto di compressione
     compressor.attack.setValueAtTime(df_att, c.currentTime);  // Imposta l'attacco
     compressor.release.setValueAtTime(df_rel, c.currentTime); // Imposta il rilascio
+}
+
+function updateThreshold(df_th){
+    compressor.threshold.setValueAtTime(df_th, c.currentTime);
+}
+
+function updateRatio(df_ratio) {
+    compressor.ratio.setValueAtTime(df_ratio, c.currentTime);
+}
+
+function updateKnee(df_knee) {
+    compressor.knee.setValueAtTime(df_knee, c.currentTime);
+}
+
+function updateAtt(df_att){
+    compressor.attack.setValueAtTime(df_att, c.currentTime);
+}
+
+function updateRel(df_rel){
+    compressor.release.setValueAtTime(df_rel, c.currentTime);
 }
 
 function suspend() {
